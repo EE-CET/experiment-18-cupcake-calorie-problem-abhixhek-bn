d512b07373752dcd22d5d2301ecd5a75856bef1e
Abhishek B <abhishekbn10991@gmail.com>
2026-01-28 15:17:16 +0530

    first commit

--- src/Cupcake.java
+++ src/Cupcake.java
@@ -1,30 +1,23 @@
-    
-
 import java.util.*;
-
 
 public class Cupcake {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        Integer[] calories = new Integer[n];
+        Integer[] a = new Integer[n];
         for (int i = 0; i < n; i++) {
-            calories[i] = sc.nextInt();
+            a[i] = sc.nextInt();
         }
 
-        // Sort in descending order
-        Arrays.sort(calories, Collections.reverseOrder());
+        // Sort descending
+        Arrays.sort(a, Collections.reverseOrder());
 
         long miles = 0;
         for (int i = 0; i < n; i++) {
-            miles += calories[i] * (long)Math.pow(2, i);
+            miles += a[i] * (long)Math.pow(2, n - 1 - i);
         }
 
         System.out.println(miles);
     }
 }
-
-
-    
-
